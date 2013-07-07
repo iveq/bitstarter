8b7f81a1941b8e4daffd138dfe29b667cc91d23c
Ivan Branica <ivan.branica@gmail.com>
2013-07-07 20:58:00 +0000

    Reading file into buffer and sending as respone

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var indexFilename = "./index.html";
 
 var app = express.createServer(express.logger());
+var buf = fs.readFileSync(indexFilename);
 
 app.get('/', function(request, response) {
+  respone.send(buf);	
+/*
   response.send('Hello World 2!');
+*/
 });
 
 var port = process.env.PORT || 5000;
